34e4c5e4ff149852792e45e7c982fab962c5819e
Mike Bostock <mbostock@gmail.com>
2018-08-24 13:50:04 -0700

    Move index; add unpkg & jsdelivr fields.

--- index.js
+++
@@ -1,7 +0,0 @@
-export {default as blob} from "./src/blob";
-export {default as buffer} from "./src/buffer";
-export {default as dsv, csv, tsv} from "./src/dsv";
-export {default as image} from "./src/image";
-export {default as json} from "./src/json";
-export {default as text} from "./src/text";
-export {default as xml, html, svg} from "./src/xml";
