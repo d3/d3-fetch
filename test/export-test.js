caecfde69e0ea5c3c6adb2c03ff7288c89908af7
Mike Bostock <mbostock@gmail.com>
2021-06-04 10:30:50 -0700

    typeof test

--- test/export-test.js
+++ test/export-test.js
@@ -2,12 +2,15 @@ import assert from "assert";
 import * as d3 from "../src/index.js";
 
 it("fetch methods", () => {
-  assert.deepStrictEqual(Object.keys(d3), [
-    'blob', 'buffer',
-    'csv',  'dsv',
-    'html', 'image',
-    'json', 'svg',
-    'text', 'tsv',
-    'xml'
-  ]);
-});	
+  assert.strictEqual(typeof d3.blob, "function");
+  assert.strictEqual(typeof d3.buffer, "function");
+  assert.strictEqual(typeof d3.csv, "function");
+  assert.strictEqual(typeof d3.dsv, "function");
+  assert.strictEqual(typeof d3.html, "function");
+  assert.strictEqual(typeof d3.image, "function");
+  assert.strictEqual(typeof d3.json, "function");
+  assert.strictEqual(typeof d3.svg, "function");
+  assert.strictEqual(typeof d3.text, "function");
+  assert.strictEqual(typeof d3.tsv, "function");
+  assert.strictEqual(typeof d3.xml, "function");
+});
